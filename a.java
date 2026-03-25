70987ca8309e94b3bd6052d460e2b37bf3f33e2b
shiva1821 <vishalmudhiraj151@gmail.com>
2026-03-25 12:36:13 +0530

    Create a.java
    added java program

---
+++ a.java
@@ -0,0 +1,18 @@
+package methods;
+
+public class a {
+	public static String name() {
+		return "shiva";
+	}
+	public static void shiva() {
+		System.out.println("i am shiva");
+	}
+	public static void main(String[] args) {
+		System.out.println(name());
+		name();
+		shiva();
+	//	name();
+	//	shiva();
+	}
+}
+
